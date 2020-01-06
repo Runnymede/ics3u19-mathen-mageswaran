79d4f5d6d78118713cd614cfd66cb41cc04230f7
342364858 <342364858@C-3ZWJYR1.tdsb.on.ca>
2020-01-06 15:05:45 -0500

    mathplus updated

--- ICS3U - Mathen Mageswaran/src/mageswaran/unit4/Mathplus.java
+++ ICS3U - Mathen Mageswaran/src/mageswaran/unit4/Mathplus.java
@@ -6,20 +6,44 @@ public class Mathplus {
 
 	}
 
-	public static int min () {
+	public static int min (int[]numbers) {
+		int min = numbers[0];
+		for(int i = 1; i<numbers.length; i++) {
+			if (numbers[i]<min) {
+				min=numbers[i];
+			}
+		}
+		return min;
+	}
+	
+	public static int max(int[]) {
 
 	}
 	
-	public static int max() {
+	public static int sum(int[]number) {
+		int sumFull = 0;
+		for (i = 0; i<sum.length; i++)
+			sumFull = sumFull + number[i];
+			
+	}
+	
+	public static double average(int[]) {
 
 	}
 	
-	public static int sum() {
+	public static double min(double[]) {
 
 	}
 	
-	public static double average() {
+	public static double max(double[]) {
 
 	}
 	
+	public static double sum(double[]) {
+
+	}
+	
+	public static double average(double[]) {
+
+	}
 }
